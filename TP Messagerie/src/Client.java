8f083ca8776110ca2166132ca1e1aecb0cc4bc68
prannou <prannou29@gmail.com>
2012-11-16 11:46:41 +0100

    Correction d'erreurs de connexion

--- TP Messagerie/src/Client.java
+++ TP Messagerie/src/Client.java
@@ -7,6 +7,7 @@
  *
  * @author jonathancozzo
  */
+import java.net.MalformedURLException;
 import java.rmi.*;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.*; // Hashtable
@@ -14,51 +15,50 @@ import java.io.*;
 
 public class Client {
 
-    static String id;
-    static int nbMessages;
+	static String id;
+	static int nbMessages;
 
-    public Client() {
-        Client.id = null;
-        Client.nbMessages = 0;
-    }
+	public Client() {
+		Client.id = null;
+		Client.nbMessages = 0;
+	}
 
-    public static void main(String args[]) {
-        try {
-            // Récupération d'un stub sur l'objet serveur.
-            Messagerie messagerie = (Messagerie) Naming.lookup("//127.0.0.1:8080/chatservice");
+	public static void main(String args[]) throws NotBoundException, IOException {
+		// Récupération d'un stub sur l'objet serveur.
+		Messagerie messagerie = (Messagerie) Naming
+				.lookup("//127.0.0.1:8080/chatservice");
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            String entree = in.readLine();
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String entree = in.readLine();
 
-            while (true) {
-                String[] entreeClavier = entree.split("\\s+");
-                if (entreeClavier[0] == "connect") {
-                    messagerie.connect(id);
-                    System.out.print("Bienvenue sur la messagerie !\n");
-                } else if (entreeClavier[0] == "send") {
-                    messagerie.send(entree.substring(5), id);
-                } else if (entreeClavier[0] == "bye") {
-                    messagerie.bye(id);
-                    System.out.print("Aurevoir !\n");
-                    break;
-                } else if (entreeClavier[0] == "who") {
-                    ArrayList<String> listeUsers = messagerie.who();
-                    System.out.print("Liste des utilisateurs connectés :\n");
-                    System.out.print(listeUsers);
-                } else {
-                    System.out.print("Erreur : veuillez entrée une commande valide\n");
-                }
-                // Rafraichissement et affichage des nouveaux messages
-                ArrayList<Map<Integer, String>> messages = messagerie.getMessages(nbMessages);
-                nbMessages += messages.get(0).size();
-                int i;
-                for (i = 0; i < messages.get(0).size(); i++) {
-                    System.out.print(messages.get(0).get(i) + " : " + messages.get(1).get(i) + "\n");
-                }
-            }
-
-        } catch (Exception exc) {
-            System.out.print("Erreur");
-        }
-    }
+		while (true) {
+			String[] entreeClavier = entree.split("\\s+");
+			if (entreeClavier[0] == "connect") {
+				messagerie.connect(id);
+				System.out.print("Bienvenue sur la messagerie !\n");
+			} else if (entreeClavier[0] == "send") {
+				messagerie.send(entree.substring(5), id);
+			} else if (entreeClavier[0] == "bye") {
+				messagerie.bye(id);
+				System.out.print("Aurevoir !\n");
+				break;
+			} else if (entreeClavier[0] == "who") {
+				ArrayList<String> listeUsers = messagerie.who();
+				System.out.print("Liste des utilisateurs connectés :\n");
+				System.out.print(listeUsers);
+			} else {
+				System.out
+						.print("Erreur : veuillez entrée une commande valide\n");
+			}
+			// Rafraichissement et affichage des nouveaux messages
+			ArrayList<Map<Integer, String>> messages = messagerie
+					.getMessages(nbMessages);
+			nbMessages += messages.get(0).size();
+			int i;
+			for (i = 0; i < messages.get(0).size(); i++) {
+				System.out.print(messages.get(0).get(i) + " : "
+						+ messages.get(1).get(i) + "\n");
+			}
+		}
+	}
 }
