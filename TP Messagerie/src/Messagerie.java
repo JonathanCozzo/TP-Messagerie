153d56c9b6f58ae24ef7362362c08dd8e9622f73
prannou <prannou29@gmail.com>
2012-11-16 09:49:51 +0100

    Complétion du main de Messagerie.java

--- TP Messagerie/src/Messagerie.java
+++ TP Messagerie/src/Messagerie.java
@@ -7,61 +7,78 @@
  *
  * @author jonathancozzo
  */
+import java.net.InetAddress;
 import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.*; // Hashtable
 
 public class Messagerie extends UnicastRemoteObject implements Serveur {
 
-    private ArrayList<String> listeUsers;
-    private Map<Integer, String> tableauId;
-    private Map<Integer, String> tableauMessages;
+	private ArrayList<String> listeUsers;
+	private Map<Integer, String> tableauId;
+	private Map<Integer, String> tableauMessages;
 
-    // Implémentation du constructeur
-    public Messagerie() throws java.rmi.RemoteException {
-        this.listeUsers = new ArrayList<String>() ;
-        this.tableauId = new HashMap<Integer, String>();
-        this.tableauMessages =new HashMap<Integer, String>();
-    }
-    
-    // Implémentation des méthodes distantes
+	// Implémentation du constructeur
+	public Messagerie() throws java.rmi.RemoteException {
+		this.listeUsers = new ArrayList<String>();
+		this.tableauId = new HashMap<Integer, String>();
+		this.tableauMessages = new HashMap<Integer, String>();
+	}
 
-    public void connect(String id) throws java.rmi.RemoteException {
-        this.listeUsers.add(id);
-    }
-    
-    public void send(String message, String id) throws java.rmi.RemoteException {
-    	this.tableauId.put(this.tableauId.size(), id);
-    	this.tableauMessages.put(this.tableauMessages.size(), message);
-    }
-    
-    public void bye(String id) throws java.rmi.RemoteException {
-    	this.listeUsers.remove(id);
-    }
-    
-    public ArrayList<String> who() throws java.rmi.RemoteException {
-    	return this.listeUsers;
-    }
-    
-    public ArrayList<Map<Integer, String>> getMessages(int nbMessages)throws java.rmi.RemoteException  {
-       	ArrayList<Map<Integer, String>> messages = new ArrayList<Map<Integer, String>>();
-    	int i;
-    	Map<Integer, String> tabId = this.tableauId;
-    	Map<Integer, String> tabMessages = this.tableauMessages;
-    	for(i=0; i<nbMessages;i++){
-    		tabId.remove(i);
-    		tabMessages.remove(i);
-    	}
-       	messages.add(0,tabId);
-    	messages.add(1,tabMessages);
-    	return messages;
-    }
+	// Implémentation des méthodes distantes
 
+	public void connect(String id) throws java.rmi.RemoteException {
+		this.listeUsers.add(id);
+	}
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+	public void send(String message, String id) throws java.rmi.RemoteException {
+		this.tableauId.put(this.tableauId.size(), id);
+		this.tableauMessages.put(this.tableauMessages.size(), message);
+	}
+
+	public void bye(String id) throws java.rmi.RemoteException {
+		this.listeUsers.remove(id);
+	}
+
+	public ArrayList<String> who() throws java.rmi.RemoteException {
+		return this.listeUsers;
+	}
+
+	public ArrayList<Map<Integer, String>> getMessages(int nbMessages)
+			throws java.rmi.RemoteException {
+		ArrayList<Map<Integer, String>> messages = new ArrayList<Map<Integer, String>>();
+		int i;
+		Map<Integer, String> tabId = this.tableauId;
+		Map<Integer, String> tabMessages = this.tableauMessages;
+		for (i = 0; i < nbMessages; i++) {
+			tabId.remove(i);
+			tabMessages.remove(i);
+		}
+		messages.add(0, tabId);
+		messages.add(1, tabMessages);
+		return messages;
+	}
+
+	public static void main(String args[]) {
+		int port; String URL;
+		try {
+		// transformation d ’une chaîne de caractères en entier
+		Integer I = new Integer(args[0]);
+		port = I.intValue();
+		} catch (Exception ex) {
+		System.out.println(" Please enter: Server <port>"); return;
+		}
+		try {
+		// Création du serveur de nom - rmiregistry
+		Registry registry = LocateRegistry.createRegistry(port);
+		// Création d ’une instance de l’objet messagerie
+		Messagerie obj = new Messagerie();
+		// Calcul de l’URL du serveur
+		URL = "//"+InetAddress.getLocalHost().getHostName()+":"+
+		port+"/mon_serveur";
+		Naming.rebind(URL, obj);
+		} catch (Exception exc) {System.out.println("Error");}
+	}
 }
